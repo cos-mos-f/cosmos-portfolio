3be64c18eb0bb7a3382226bd65395ffa4f2fe0a8
cos-mos-f <fengyuejiangcun@gmail.com>
2024-11-14 11:40:49 +0900

    a

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,14 @@
 import type { NextConfig } from 'next';
 
 const isProd = process.env.NODE_ENV === 'production';
+const branchName = process.env.BRANCH_NAME ? "/" + process.env.BRANCH_NAME : "";
 
 const nextConfig: NextConfig = {
-  assetPrefix: isProd ? '/cosmos-portfolio/' : '',
-  basePath: isProd ? '/cosmos-portfolio' : '',
+  // assetPrefix: isProd ? '/cosmos-portfolio/' : '',
+  // basePath: isProd ? '/cosmos-portfolio' : '',
+  reactStrictMode: true,
+  assetPrefix: branchName,
+  basePath: branchName,
   output: 'export',
 };
 
