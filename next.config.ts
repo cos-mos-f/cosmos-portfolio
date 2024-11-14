2a64205c37c9caaa066f5728eba20359fc302625
cos-mos-f <fengyuejiangcun@gmail.com>
2024-11-14 10:32:13 +0900

    a

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: isProd ? '/cosmos-portfolio/' : '',
+  basePath: isProd ? '/cosmos-portfolio' : '',
+  output: 'export',
 };
 
 export default nextConfig;
+
