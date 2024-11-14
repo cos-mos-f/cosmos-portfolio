38a7846bb33b020eba3fac754de4b3ea1d4c9b7a
cos-mos-f <fengyuejiangcun@gmail.com>
2024-11-15 00:19:12 +0900

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@
 import styles from './styles/Home.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>Welcome to My Portfolio</h1>
-      <button className={styles.button}>Click Me</button>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <h1>Welcome to My Portfolio</h1>
+            <button className={styles.button}>Click Me</button>
+        </div>
+    );
 }
 
 
