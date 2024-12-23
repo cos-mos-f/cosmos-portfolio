bf5ff07375f408a96accc279a9699f58cef347ee
cos-mos-f <fengyuejiangcun@gmail.com>
2024-12-23 23:31:17 +0900

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import ScrollBar from './components/scrollBar';
 import ArtBoard from './components/artBoard';
 
 const imageList = [
-  { title: 'Image 1', path: '../images/fan_arts/main/2024.12.63.02.jpg' },
-  { title: 'Image 2', path: '../images/fan_arts/main/2024.12.64.1.jpg' },
-  { title: 'Image 3', path: '../images/fan_arts/main/2024.12.65.1.jpg' },
-  { title: 'Image 4', path: '../images/fan_arts/main/2024.12.86.jpg' },
+  { title: 'Image 1', path: '/images/fan_arts/main/2024.12.63.02.jpg' },
+  { title: 'Image 2', path: '/images/fan_arts/main/2024.12.64.1.jpg' },
+  { title: 'Image 3', path: '/images/fan_arts/main/2024.12.65.1.jpg' },
+  { title: 'Image 4', path: '/images/fan_arts/main/2024.12.86.jpg' },
 ];
 
 export default function Home() {
