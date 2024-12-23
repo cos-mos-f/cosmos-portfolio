5e694b613237f7d9183e955a71f21e075806f41d
cos-mos-f <fengyuejiangcun@gmail.com>
2024-12-23 23:41:28 +0900

    a

--- src/app/components/artBoard.tsx
+++ src/app/components/artBoard.tsx
@@ -6,11 +6,13 @@ interface ArtBoardProps {
 }
 
 const ArtBoard: React.FC<ArtBoardProps> = ({ image }) => {
+  const base = process.env.GITHUB_PAGES ? '/cosmos-portfolio/' : './';
+  
   return (
     <div className={styles.artBoard}>
       <div className={styles.title}>{image.title}</div>
       <div className={styles.artFrame}>
-      <img src={image.path} alt={image.title} className={styles.image} />
+      <img src={base+image.path} alt={image.title} className={styles.image} />
       </div>
     </div>
   );
